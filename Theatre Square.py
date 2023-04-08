c9f6fee516e9c799afa38b32fecc3522310c6fbb
Kartikey Singh <85943021+QuantuM410@users.noreply.github.com>
2023-04-08 19:21:24 +0530

    Update Theatre Square.py

--- Theatre Square.py
+++ Theatre Square.py
@@ -1,13 +1,5 @@
-n, m, a = map(int, input().split())
+from math import ceil
 
-if n % a != 0 and a < n:
-    n = n + (n-a)
-else:
-    a1 = a - (a-n)
-
-if m % a != 0 and a < m:
-    m = m + (m-a)
-else:
-    a2 = a - (a-m)
-
-print((n*m)//(a1*a2))
+n, m, a = map(int,input().split())
+ 
+print(int(ceil(n/a)*ceil(m/a)))
